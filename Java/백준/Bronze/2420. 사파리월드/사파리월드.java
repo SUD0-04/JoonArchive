075c3feb7e7cdd57bc6d2685f89ea2d9bc0ba4d7
Kngmin Kang <kngmin04@naver.com>
2026-02-04 23:47:43 +0900

    [Bronze V] Title: 사파리월드, Time: 160 ms, Memory: 17600 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+        
+        long result = Math.abs(N-M);
+        
+        System.out.println(result);
+        
+        sc.close();
+    }
+}
