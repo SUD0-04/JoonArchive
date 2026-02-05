0376d91cde02a693bd9a82ba71ea027e69498c0b
Kngmin Kang <kngmin04@naver.com>
2026-02-06 00:10:11 +0900

    [Bronze V] Title: Pyramids, Time: 148 ms, Memory: 17568 KB -BaekjoonHub

---
+++ Java/백준/Bronze/5341. Pyramids/Pyramids.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int floor = sc.nextInt();
+
+            if (floor == 0) {
+                break;
+            }
+
+            int total = 0;
+            
+            for (int i = 1; i <= floor; i++) {
+                total += i;
+            }
+
+            System.out.println(total);
+        }
+
+        sc.close();
+    }
+}
