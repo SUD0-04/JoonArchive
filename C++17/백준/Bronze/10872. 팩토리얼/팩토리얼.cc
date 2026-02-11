44ec99993e47f083c7565201859c122363a4eed0
Kngmin Kang <kngmin04@naver.com>
2026-02-11 23:15:08 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+long long factorial(int N) {
+    if (N <= 1) return 1;
+    return N * factorial(N - 1);
+}
+
+int main (void) {
+    int N;
+    
+    cin >> N;
+    
+    cout << factorial(N) << endl;
+    
+    return 0;
+}
