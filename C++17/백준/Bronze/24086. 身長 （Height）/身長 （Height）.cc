49b1cf37d768c0d0969ca3cbbfff9f5b09dd9091
Kang Kyoung Min <kngmin04@naver.com>
2026-02-23 20:33:15 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (void) {
+    long long A, B;
+    
+    cin >> A >> B;
+    
+    cout << abs(A - B) << endl;
+    
+    return 0;
+}
