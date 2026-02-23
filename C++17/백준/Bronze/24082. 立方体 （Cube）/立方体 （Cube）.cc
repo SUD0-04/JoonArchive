1821df6e77c32241a0500c90234596b2a6c36c28
Kang Kyoung Min <kngmin04@naver.com>
2026-02-23 20:29:08 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (void) {
+    long long n;
+    
+    cin >> n;
+    
+    cout << (long long) pow (n, 3) << endl;
+    
+    return 0;
+}
