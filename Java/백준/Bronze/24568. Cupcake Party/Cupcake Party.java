bee1335441352721cda9b03e4d77c9c3a1353ccf
Kang Kyoung Min <kngmin04@naver.com>
2026-02-24 19:38:05 +0900

    [Bronze V] Title: Cupcake Party, Time: 176 ms, Memory: 17588 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        int R = sc.nextInt();
+        int S = sc.nextInt();
+        
+        System.out.println((R * 8) + (S * 3) - 28);
+        
+        sc.close();
+    }
+}
