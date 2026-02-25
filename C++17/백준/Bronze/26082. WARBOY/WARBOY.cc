0a5689187ca56395cecf4ec23bc4e09cc40f9f0f
Kang Kyoung Min <kngmin04@naver.com>
2026-02-25 22:59:51 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (void) {
+    int a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    int result = b / a * c * 3;
+    
+    cout << result << endl;
+    
+    return 0;
+}
