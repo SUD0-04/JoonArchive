71e320e9861e124e8169ac6ec38663d14ed636e3
Kang Kyoung Min <kngmin04@naver.com>
2026-02-27 20:19:38 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (void) {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        long long n2;
+        cin >> n2;
+        
+        cout << n2 << " " << n2 << endl;
+    }
+    
+    return 0;
+}
