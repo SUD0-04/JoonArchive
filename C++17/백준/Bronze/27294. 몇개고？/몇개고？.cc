ee9f734e2159a6995362537af5dff71dd68a6db9
Kang Kyoung Min <kngmin04@naver.com>
2026-02-28 00:25:38 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (void) {
+    int T, S;
+    cin >> T >> S;
+    
+    if (T >= 12 && T <= 16 && S == 0) {
+        cout << 320 << endl;
+    } else {
+        cout << 280 << endl;
+    }
+    
+    return 0;
+}
