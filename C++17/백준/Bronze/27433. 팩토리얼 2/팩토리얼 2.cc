9580db9bb0393da6ab2a4bada5245c5ac851de6e
Kang Kyoung Min <kngmin04@naver.com>
2026-03-03 23:00:41 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long result = 1;
+
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    cout << result;
+
+    return 0;
+}
