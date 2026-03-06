3add33530e2e660d7d8b3b69b992216ac4067546
Kang Kyoung Min <kngmin04@naver.com>
2026-03-06 12:07:43 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (void) {
+    int s1, s2;
+    cin >> s1 >> s2;
+    
+    if (s1 * 2 >= s2) {
+        cout << "E";
+    } else {
+        cout << "H";
+    }
+    
+    return 0;
+}
