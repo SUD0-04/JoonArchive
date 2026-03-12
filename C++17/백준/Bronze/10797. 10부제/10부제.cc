ab456b37ece68340dfc6359a0f325a33eba6be59
Kang Kyoung Min <kngmin04@naver.com>
2026-03-12 23:58:10 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+    
+    int count = 0;
+    
+    for (int i = 0; i < 5; i++) {
+        int cnum;
+        cin >> cnum;
+        
+        if (cnum == n) {
+            count++;
+        }
+    }
+    
+    cout << count;
+    
+    return 0;
+}
