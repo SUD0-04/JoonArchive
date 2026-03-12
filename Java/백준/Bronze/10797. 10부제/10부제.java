1fbd6bd84deaf28974e780df49a9436800390623
Kang Kyoung Min <kngmin04@naver.com>
2026-03-13 00:00:30 +0900

    [Bronze IV] Title: 10부제, Time: 180 ms, Memory: 17736 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        int num = sc.nextInt();
+        
+        int count = 0;
+        
+        for (int i = 0; i < 5; i++) {
+            int cnum = sc.nextInt();
+            if (cnum == num) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+        
+        sc.close();
+    }
+}
