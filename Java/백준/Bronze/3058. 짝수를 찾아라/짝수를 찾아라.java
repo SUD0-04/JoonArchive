ec3ac25d51fb48dc1547f7e574e5c7a20da47380
Kang Kyoung Min <kngmin04@naver.com>
2026-03-21 01:25:14 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 352 ms, Memory: 28328 KB -BaekjoonHub

---
+++ Java/백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            int min = Integer.MAX_VALUE;
+
+            for (int j = 0; j < 7; j++) {
+                int num = sc.nextInt();
+
+                if (num % 2 == 0) {
+                    sum += num;
+                    if (num < min) {
+                        min = num;
+                    }
+                }
+            }
+            System.out.println(sum + " " + min);
+        }
+
+        sc.close();
+    }
+}
