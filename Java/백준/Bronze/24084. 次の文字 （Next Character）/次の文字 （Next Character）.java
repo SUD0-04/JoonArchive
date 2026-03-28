5743e99a1185d4749dfa6d896952f5988edfb05b
Kang Kyoung Min <kngmin04@naver.com>
2026-03-28 13:49:37 +0900

    [Bronze IV] Title: 次の文字 (Next Character), Time: 176 ms, Memory: 17740 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24084. 次の文字 （Next Character）/次の文字 （Next Character）.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        int n = sc.nextInt();
+        String s = sc.next();
+        
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == 'J') {
+                System.out.println(s.charAt(i - 1));
+            }
+        }
+        
+        sc.close();
+    }
+}
